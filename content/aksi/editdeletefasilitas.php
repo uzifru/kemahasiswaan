aeaff44cdbca095b9bd0b5b931cb7b4045926c02
Yudaka <dakala@protonmail.ch>
2020-01-22 16:11:31 +0700

    [+] memperbarui menu fasilitas [+] menambah keamanan menu fasilitas [+] memperbarui fungsi_rupiah.php

--- content/aksi/editdeletefasilitas.php
+++ content/aksi/editdeletefasilitas.php
@@ -9,46 +9,78 @@ if (isset($_SESSION['username']) and ($_SESSION['password'])):
     session_start();
     $id = $_SESSION['id'];
 
-    $kode = @$_GET['fsId'];
+    $kode = filter_var(@$_GET['fsId'], FILTER_SANITIZE_NUMBER_INT);
     $aksi = $_GET['aksi'];
     $aslina = $_GET['aslina'];
 
-    if($_POST['posting']):
+    if(isset($_POST['posting'])
+    && isset($_POST['fsNama'])
+    && isset($_POST['fsDeskripsi'])):
         if($aksi == '' and $kode != ''):
-                $namafs = $_POST['fsNama'];
-                $deskripsi = $_POST['fsDeskripsi'];
+            $namafs = $_POST['fsNama'];
+            $deskripsi = $_POST['fsDeskripsi'];
 
-                // Baca lokasi file sementar dan nama file dari form (fupload)
-                    $lokasi_file = $_FILES['fupload']['tmp_name'];
-                    $nama_file   = $_FILES['fupload']['name'];
-                    // Tentukan folder untuk menyimpan file
-                    $folder = "../../images/fasilitas/$nama_file";
-                    // Apabila file berhasil di upload
-                    if (move_uploaded_file($lokasi_file,"$folder")):
+            // Baca lokasi file sementar dan nama file dari form (fupload)
+            $lokasi_file = $_FILES['fupload']['tmp_name'];
+            $nama_file   = $_FILES['fupload']['name'];
+            // Tentukan folder untuk menyimpan file
+            $folder = "../../images/fasilitas/";
+            // Apabila file berhasil di upload
+            if (move_uploaded_file($lokasi_file, $folder . $nama_file)):
 
-                    endif;
+            endif;
             
             if($nama_file == ''):
-                $query =  $con->query("update fasilitas set
-                fsNama  = '$namafs',
-                fsDeskripsi  = '$deskripsi'
-                where fsId = $kode
-                ");
+                $u_fasilitas = $con->prepare("UPDATE fasilitas SET
+                fsNama      = ?,
+                fsDeskripsi = ?
+                WHERE fsId  = ?");
+                $u_fasilitas->bind_param('ssi', $namafs, $deskripsi, $kode);
             else:
-                $query =  $con->query("update fasilitas set
-                fsNama  = '$namafs',
-                fsDeskripsi  = '$deskripsi',
-                fsFoto = '$nama_file'
+                $q_dulu = $con->prepare("SELECT fsFoto FROM fasilitas WHERE fsId = ?");
+                $q_dulu->bind_param('i', $kode);
+                $q_dulu->execute();
+                $file_dulu = $q_dulu->get_result()->fetch_assoc();
+                // Hapus foto dulu
+                unlink($folder . $file_dulu['fsFoto']);
+                $q_dulu->free_result();
 
-                where fsId = $kode
-                ");
+                $u_fasilitas = $con->prepare("UPDATE fasilitas SET
+                fsNama      = ?,
+                fsDeskripsi = ?,
+                fsFoto      = ?
+                WHERE fsId  = ?");
+                $u_fasilitas->bind_param('sssi', $namafs, $deskripsi, $nama_file, $kode);
+
             endif;
+            $u_fasilitas->execute();
+            // Ambil data kategori
+            $kategori = $con->query("SELECT trKpId as ID FROM r_kategori_peminjaman ORDER BY ID ASC");
+            while ($kat = $kategori->fetch_assoc()) {
+                $kategori_id[] = $kat['ID'];
+            }
+
+            $u_harga = $con->prepare("UPDATE harga_sewa SET
+            hsHarga     = ?
+            WHERE (hsFasilitas = ? AND hsKategori = ?)");
+            // Update bea sewa
+            foreach($kategori_id as $k_id) {
+                $u_harga->bind_param('iii', $_POST['k_' . $k_id], $kode, $k_id);
+                $u_harga->execute();
+            }
+            $u_harga->close();
             header('Location: ../../home.php?page=fasilitas');
         endif;
     elseif($aksi = 'hapus' and $id != ''):
-            $query =  $con->query("DELETE FROM fasilitas
-            where fsId = $kode
-            ");
+            $g_foto = $con->query("SELECT fsFoto FROM fasilitas WHERE fsId = $kode");
+            $d_foto = $g_foto->fetch_assoc();
+            $d_fs = "DELETE FROM fasilitas WHERE fsId = $kode";
+            $d_hs = "DELETE FROM harga_sewa WHERE hsFasilitas = $kode";
+            // Hapus foto
+            if ($con->query($d_fs)) {
+                unlink($d_foto['fsFoto']);
+                $con->query($d_hs);
+            }
             header('Location: ../../home.php?page=fasilitas');
     endif;
 ?>
